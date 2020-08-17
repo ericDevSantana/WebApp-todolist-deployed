e9c0efe84dc99a67664d9dcfa0724e3a75012332
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-08-17 14:49:50 -0700

    Add gitignore, procfile and update

--- app.js
+++ app.js
@@ -170,7 +170,11 @@ app.post("/", (req, res) => {
 
 });
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
 // listening to port 3000
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(port, () => {
+    console.log("Server started!");
 });
